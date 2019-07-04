afa4db0e321e0eeda4162f3896380e2e75428352
EUReKACamper17 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-04 12:35:52 -0700

    Create ProgrammingIntro.java
    added to cap/madi

---
+++ Projects/CaptainAmerica/ProgrammingIntro.java
@@ -0,0 +1,24 @@
+public class ProgrammingIntro { 
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int hello = 11;
+        double unicorns = 13.99;
+        boolean watermelons = true;
+        String strawberry = "strawberrys are my favorite friut";
+
+
+        System.out.println(hello);
+        System.out.println(unicorns);
+        System.out.println(watermelons);
+        System.out.println(strawberry);
+    }
+}
+
+
+
+
+
+
+
