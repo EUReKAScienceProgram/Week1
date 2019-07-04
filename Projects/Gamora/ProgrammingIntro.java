6dbce38129aa41e8e5a69b9b6e0ee14f2b37ab23
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-04 12:52:17 -0700

    Add programming file to gamora - Reed

---
+++ Projects/Gamora/ProgrammingIntro.java
@@ -0,0 +1,69 @@
+public class ProgrammingIntro {
+
+    public static void main(String[] args){
+        System.out.println("Hello, dumbest dog in the Milky Way.");
+
+        int variableDoge = 420;
+        double variableDumbDog = 2.48;
+        boolean MicaIsSmart = false;
+        boolean MicaIsDumb = true;
+        String fromAspider = "I am blessed with the dumbest dog of all time, and if not, in the top three for sure.";
+        
+        System.out.println(variableDoge);
+        System.out.println(variableDumbDog);
+        System.out.println(MicaIsSmart);
+        System.out.println(MicaIsDumb);
+        System.out.println(fromAspider);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
