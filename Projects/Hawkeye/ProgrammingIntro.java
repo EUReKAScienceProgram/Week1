1b68f54d7cfd90fc6f70ecdfeddd7aa93359b057
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-04 12:55:56 -0700

    Added programming file to Hawkeye - Reed

---
+++ Projects/Hawkeye/ProgrammingIntro.java
@@ -0,0 +1,19 @@
+public class ProgrammingIntro {
+    
+    public static void main(String[] args){
+        System.out.println("Hello Dubu, Rika and Coco");
+
+        int dubuVariable = 72;
+        double rikaVariable = 0.10; 
+        boolean cocoIsFat = true;
+        String CatWand = "My cat Coco is very fat and plays with a cat wand";
+
+
+        System.out.println(CatWand);
+        System.out.println(cocoIsFat);
+        System.out.println(rikaVariable);
+        System.out.println(dubuVariable);
+    }
+
+
+}
