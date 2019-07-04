5888d3410f153bcb5e58bda0b434331d53df2455
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-04 12:39:08 -0700

    Added programming file to Thor - Reed

---
+++ Projects/Thor/ProgrammingIntro.java
@@ -0,0 +1,21 @@
+public class ProgrammingIntro {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    
+        int vbux = 10;
+        double ikonikskin = 666.666;
+        boolean itstrue  = true;
+        String battlebus = "land dusty divot or mega mall in fortnite";
+
+        System.out.println(vbux);
+        System.out.println(ikonikskin);
+        System.out.println(itstrue);
+        System.out.println(battlebus);
+
+
+    }
+
+
+
+}
